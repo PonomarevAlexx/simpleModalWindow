c03285e995397a911800102453aad26edfed73ce
Alex Ponomarev <ponomar3v.alexey@yandex.ru>
2023-06-21 16:27:46 +0300

    v 1.0

---
+++ script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+function modal(trigerSelector, closeSelector, overlaySelector, modalSelector) {
+
+    const btnShowModals = document.querySelectorAll(trigerSelector),
+          btnCloseModal = document.querySelector(closeSelector),
+          overlay = document.querySelector(overlaySelector),
+          modal = document.querySelector(modalSelector);
+
+    function showModal() {
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    };
+
+    function closeModal() {
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    };
+
+    btnShowModals.forEach(item => {
+        item.addEventListener('click', () =>{
+            showModal();
+        })
+    });
+
+    btnCloseModal.addEventListener('click', () => {
+        closeModal();
+    });
+
+    document.addEventListener('click', (e) => {
+        if(e.target === overlay) closeModal();
+    });
+}
+
+modal ('.show-modal-window', '.close-modal-window', '.overlay', '.modal-window')
